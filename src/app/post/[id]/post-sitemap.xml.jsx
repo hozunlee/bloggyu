6925d22bfa4e7fead7173136a896bc48970684b2
hozunlee <ho2yahh@gmail.com>
2023-07-07 13:53:55 +0900

    sitemap 수정

--- src/app/post/[id]/post-sitemap.xml.jsx
+++ src/app/post/[id]/post-sitemap.xml.jsx
@@ -11,7 +11,7 @@ export const getServerSideProps = async (context) => {
 
     const sitemapFields = post.data.map((item) => {
         return {
-            loc: `http://localhost:8088/product/${item.id}`, // 페이지 경로
+            loc: `${process.env.NEXT_PUBLIC_BLOG_URL}/${item.id}`, // 페이지 경로
             lastmod: new Date().toISOString(), // 최근변경일자
             changefreq: "daily", // 페이지 주소 변경 빈도 (검색엔진에 제공됨) - always, daily, hourly, monthly, never, weekly, yearly 중 택 1
             priority: 1, // 페이지 주소 우선순위 (검색엔진에 제공됨, 우선순위가 높은 순서대로 크롤링함)
