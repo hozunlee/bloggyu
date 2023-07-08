1ef051e654f4430608aab9b352927fd3716bf656
hozunlee <ho2yahh@gmail.com>
2023-07-09 02:20:01 +0900

    캐싱 처리

--- next.config.js
+++ next.config.js
@@ -3,6 +3,19 @@ const nextConfig = {
     images: {
         domains: ["pcpoarzixkpbnromuhrh.supabase.co"],
     },
+    async headers() {
+        return [
+            {
+                source: "/",
+                headers: [
+                    {
+                        key: "Cache-Control",
+                        value: "private, no-cache, no-store, max-age=0, must-revalidate",
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 module.exports = nextConfig;
