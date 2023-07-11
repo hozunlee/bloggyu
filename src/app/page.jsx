cda217203840b3a37a7422f420ab93ad67f54374
hozunlee <ho2yahh@gmail.com>
2023-07-11 23:47:17 +0900

    main 에서 패칭

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,30 +1,39 @@
-import Script from "next/script";
+import { fetchAPI } from "@/lib/api";
+import Cards from "@/lib/components/Cards";
 import MainPage from "@/lib/components/MainPage";
 
-// async function getData() {
-//     const res = await fetchAPI("/posts", {
-//         populate: ["main_cover"],
-//         sort: ["id:desc"],
-//     });
+export const fetchCache = "force-no-store";
+export const revalidate = 0; // seconds
+export const dynamic = "force-dynamic";
 
-//     // const res2 = await axios(
-//     //     `${
-//     //         process.env.NEXT_PUBLIC_STRAPI_API_URL || "http://localhost:1337"
-//     //     }/api/posts?populate=*`
-//     // );
+async function getData() {
+    const res = await fetchAPI("/posts", {
+        populate: ["main_cover"],
+        sort: ["id:desc"],
+    });
 
-//     // return { props: { posts: res2.data.data } };
-//     return { props: { posts: res } };
-// }
+    //     // const res2 = await axios(
+    //     //     `${
+    //     //         process.env.NEXT_PUBLIC_STRAPI_API_URL || "http://localhost:1337"
+    //     //     }/api/posts?populate=*`
+    //     // );
+
+    //     // return { props: { posts: res2.data.data } };
+    return { props: { posts: res } };
+}
 
 export default async function Page() {
-    // const {
-    //     props: { posts },
-    // } = await getData();
+    const {
+        props: { posts },
+    } = await getData();
 
     return (
         <div>
-            <MainPage />
+            {posts.map((card, i) => (
+                <div key={card.publishedAt}>
+                    <Cards card={card} />
+                </div>
+            ))}
         </div>
     );
 }
